3a2ec788c5ce5966b361999c3736253f54132044
Abhishek <pampariabhishek32378@gmail.com>
2025-09-19 13:02:13 +0530

    some message

---
+++ Hello-world.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# this is comment, cany be executed, just for information purpose
+echo "Hello World"
+echo "How are you"
---
+++ conversation.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "ramesh:: Hi suresh, how are you?"
+echo "suresh:: Hello Ramesh, I am fine. How are you"
