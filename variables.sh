1440c3fc99cb5d12f7598cd16baa36e4d3e042b7
Abhishek <pampariabhishek32378@gmail.com>
2025-09-19 13:16:37 +0530

    variable message

---
+++ variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#no space before or after equal
+PERSON1=JOHN
+PERSON2=RAHUL
+
+echo "$PERSON1:: Hi $PERSON2, how are you?"
+echo "$PERSON2:: Hello $PERSON1sh, I am fine. How are you"
