edbbcabbb8b709b3ee5b64c9da1eb09660733072
Abhishek <pampariabhishek32378@gmail.com>
2025-09-22 22:57:07 +0530

    conditionals

--- installation.sh
+++ installation.sh
@@ -4,12 +4,14 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]; then
     echo "error:: please run this script with root privilages"
+    exit 1 
 fi
 
 dnf install mysql -y
 
 if [ $? -ne 0 ]; then
     echo "error:: Installing mysql is failure"
+    exit 1
 else
     echo "installing mysql is sucess"
 fi
