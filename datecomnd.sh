3df865a0916149a8747f6de7c9abefc7fbdd7361
Abhishek <pampariabhishek32378@gmail.com>
2025-09-22 08:51:30 +0530

    date

--- datecomnd.sh
+++ datecomnd.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-DATE=$(date) 
+#DATE=$(date) 
 
-echo "Timestamp executed: $DATE"
+Start_Time=$(DATE +%s)
+
+sleep 10 &
+
+End_Time=$(DATE +%s)
+
+Total_Time=$(End_Time-$Start_Time)
+
+echo "Script executed in: $Total_Time in seconds"
