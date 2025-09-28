fbbd2869291892ea09e8311052322f374da69f11
Abhishek <pampariabhishek32378@gmail.com>
2025-09-28 15:28:38 +0530

    set

--- set.sh
+++ set.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 set -e    #-e means exit when error occurs
+error(){
+    echo "there is and error"
+}
+
+trap error ERR
 echo "Hello.."
 echo "Before error.."
-ccaffjl;dnf # here shell understands there is an error and signal is ERR
+ccaffjl;dnf # here shell understands there is an error and send signal 'ERR' then automatically Trap command will execute
 echo "After error"
