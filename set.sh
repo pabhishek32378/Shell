5a6941d0b05beca6d2a178b4477d659b4d5ee3e3
Abhishek <pampariabhishek32378@gmail.com>
2025-09-28 15:41:29 +0530

    set

--- set.sh
+++ set.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 set -e    #-e means exit when error occurs
-error(){
-    echo "there is an error in $LINENO, Command is: $BASH_COMMAND"
-}
+# error(){
+#    echo "there is an error in $LINENO, Command is: $BASH_COMMAND"
+# }
 
-trap error ERR
+trap 'echo "there is an error in $LINENO, Command is: $BASH_COMMAND"' ERR
 echo "Hello.."
 echo "Before error.."
 ccaffjl;dnf # here shell understands there is an error and send signal 'ERR' then automatically Trap command will execute
