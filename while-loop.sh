a94f1605e03b4a9c9be6e11486d80ea43a298ed0
Abhishek <pampariabhishek32378@gmail.com>
2025-10-03 21:32:19 +0530

    message

--- while-loop.sh
+++ while-loop.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
-count=5
+# count=5
 
-echo "Starting countdown..."
+# echo "Starting countdown..."
 
-while [ $count -gt 0 ]
-do
-   echo "Time left: $count"
-   sleep 1 # Pause for 1 second
-   count=$((count - 1)) # Decrement the count
-done
+# while [ $count -gt 0 ]
+# do
+#   echo "Time left: $count"
+#   sleep 1 # Pause for 1 second
+#   count=$((count - 1)) # Decrement the count
+# done
 
-echo "Times up!"
+# echo "Times up!"
+
+while IFS= read -r line; do
+    # Process each line here
+    echo "Processing line: $line"
+    
+done < script-1.sh
 
